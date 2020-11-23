caaed792bba7b31fc16fcef3601b1dfe63a29838
Max Delaney <maxdelaney16@gmail.com>
2020-11-23 10:21:33 +0000

    First commit of Chess AI

---
+++ Move.java
@@ -0,0 +1,21 @@
+class Move extends ChessProject{
+  Square start;
+  Square landing;
+
+  public Move(Square x, Square y){
+    start = x;
+    landing = y;
+  }
+
+  public Move(){
+    
+  }
+
+  public Square getStart(){
+    return start;
+  }
+
+  public Square getLanding(){
+    return landing;
+  }
+}
