bbf249787ec0923d463df3843ebd37020ff70052
Max Delaney <maxdelaney16@gmail.com>
2020-12-03 22:24:34 +0000

    Added move selection menu

--- AIAgent.java
+++ AIAgent.java
@@ -20,12 +20,26 @@ public class AIAgent {
   }
 
   public Move nextBestMove(Stack possibilities){
-    Move selectedMove = new Move();
+    int moveID = rand.nextInt(possibilities.size());
+    System.out.println("Agent randomly selected move : "+moveID);
+    for(int i=1;i < (possibilities.size()-(moveID));i++){
+      possibilities.pop();
+    }
+    Move selectedMove = (Move)possibilities.pop();
     return selectedMove;
+    //Move selectedMove = new Move();
+    //return selectedMove;
   }
 
   public Move twoLevelsDeep(Stack possibilities){
-    Move selectedMove = new Move();
+    int moveID = rand.nextInt(possibilities.size());
+    System.out.println("Agent randomly selected move : "+moveID);
+    for(int i=1;i < (possibilities.size()-(moveID));i++){
+      possibilities.pop();
+    }
+    Move selectedMove = (Move)possibilities.pop();
     return selectedMove;
+    //Move selectedMove = new Move();
+    //return selectedMove;
   }
 }
